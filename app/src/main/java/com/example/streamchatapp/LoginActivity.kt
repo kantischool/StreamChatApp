03a807ac031088b0c43e04d8cd3860472d4dd96d
kanti_s_sonagra <kantisonagra7807@gmail.com>
2023-04-28 23:04:20 +0530

    feat: login funcationality implemented

--- app/src/main/java/com/example/streamchatapp/LoginActivity.kt
+++ app/src/main/java/com/example/streamchatapp/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.streamchatapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -25,11 +27,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.example.streamchatapp.ui.theme.StreamChatAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class LoginActivity : ComponentActivity() {
+
+    private val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        readTheEvents()
         setContent {
             StreamChatAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -42,75 +52,118 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun LoginScreen() {
-    var username = remember {
-        mutableStateOf(TextFieldValue(""))
-    }
+    private fun readTheEvents() {
+        lifecycleScope.launchWhenStarted {
+            loginViewModel.loginEvent.collect { event ->
+                when (event) {
+                    is LoginViewModel.LogInEvent.ErrorInputTooShort -> {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "user id is too short",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
 
-    val showProgress = remember {
-        mutableStateOf(false)
-    }
+                    is LoginViewModel.LogInEvent.ErrorLogIn -> {
+                        Toast.makeText(this@LoginActivity, event.error, Toast.LENGTH_SHORT).show()
+                    }
 
-    Column {
-
-        OutlinedTextField(
-            value = username.value,
-            onValueChange = {
-                username.value = it
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(15.dp)
-                .wrapContentHeight(),
-            label = {
-                Text(text = "User name")
+                    is LoginViewModel.LogInEvent.Success -> {
+                        Toast.makeText(this@LoginActivity, "Successful", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
-        )
-
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp)
-                .padding(15.dp)
-
-        ) {
-            Text(text = "Login as user")
         }
 
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp)
-                .padding(15.dp)
+    }
 
-        ) {
-            Text(text = "Login as guest")
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun LoginScreen() {
+        var username = remember {
+            mutableStateOf(TextFieldValue(""))
         }
 
-        if (showProgress.value)
-            CircularProgressIndicator(
+        val showProgress = remember {
+            mutableStateOf(false)
+        }
+
+        loginViewModel.loadingState.observe(this, Observer {loadingStatus ->
+
+        when(loadingStatus){
+            is LoginViewModel.LoadingState.Loading -> {
+                showProgress.value = true
+            }
+            is LoginViewModel.LoadingState.NotLoading -> {
+                showProgress.value = false
+            }
+        }
+
+        })
+        Column {
+
+            OutlinedTextField(
+                value = username.value,
+                onValueChange = {
+                    username.value = it
+                },
                 modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .width(50.dp)
-                    .height(50.dp)
-                    .padding(8.dp),
-                strokeWidth = 3.dp
+                    .fillMaxWidth()
+                    .padding(15.dp)
+                    .wrapContentHeight(),
+                label = {
+                    Text(text = "User name")
+                }
             )
 
+            Button(
+                onClick = {
+                    loginViewModel.loginUser(
+                        username.value.text,
+                        getString(R.string.jwt_token)
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .padding(15.dp)
 
+            ) {
+                Text(text = "Login as user")
+            }
+
+            Button(
+                onClick = { loginViewModel.loginUser(username.value.text) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .padding(15.dp)
+
+            ) {
+                Text(text = "Login as guest")
+            }
+
+            if (showProgress.value)
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .width(50.dp)
+                        .height(50.dp)
+                        .padding(8.dp),
+                    strokeWidth = 3.dp
+                )
+
+
+        }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     StreamChatAppTheme {
-        LoginScreen()
+//        LoginScreen()
     }
 }
--- app/src/main/java/com/example/streamchatapp/LoginViewModel.kt
+++ app/src/main/java/com/example/streamchatapp/LoginViewModel.kt
@@ -1,10 +1,99 @@
 package com.example.streamchatapp
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.models.User
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(val chatClient: ChatClient): ViewModel() {
+class LoginViewModel @Inject constructor(private val chatClient: ChatClient) : ViewModel() {
+
+    private val _loginEvent = MutableSharedFlow<LogInEvent>()
+    val loginEvent = _loginEvent.asSharedFlow()
+
+    private val _loadingState = MutableLiveData<LoadingState>()
+    val loadingState: LiveData<LoadingState> get() = _loadingState
+    private fun isUserValid(userName: String): Boolean {
+        return userName.length > 3
+    }
+
+    fun loginUser(userName: String, token: String? = null) {
+        val trimUser = userName.trim()
+
+        viewModelScope.launch {
+            if (isUserValid(userName) && token != null) {
+                loginRegisterUser(userName, token)
+            } else if (isUserValid(userName) && token == null) {
+                loginGuestUser(userName)
+            } else {
+                _loginEvent.emit(LogInEvent.ErrorInputTooShort)
+            }
+        }
+
+    }
+
+    private fun loginGuestUser(userName: String) {
+
+        _loadingState.value = LoadingState.Loading
+        chatClient.connectGuestUser(userId = userName, username = userName).enqueue {
+            _loadingState.value = LoadingState.NotLoading
+
+            if (it.isSuccess) {
+                viewModelScope.launch {
+                    _loginEvent.emit(LogInEvent.Success)
+                }
+
+            } else {
+                viewModelScope.launch {
+                    _loginEvent.emit(
+                        LogInEvent.ErrorLogIn(
+                            it.error().message ?: "Something went wrong"
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    private fun loginRegisterUser(userName: String, token: String) {
+
+        val user = User(id = userName, name = userName)
+        _loadingState.value = LoadingState.Loading
+        chatClient.connectUser(user, token = token).enqueue {
+            _loadingState.value = LoadingState.NotLoading
+            if (it.isSuccess) {
+                viewModelScope.launch {
+                    _loginEvent.emit(LogInEvent.Success)
+                }
+
+            } else {
+                viewModelScope.launch {
+                    _loginEvent.emit(
+                        LogInEvent.ErrorLogIn(
+                            it.error().message ?: "Something went wrong"
+                        )
+                    )
+                }
+            }
+        }
+
+    }
+
+    sealed class LogInEvent {
+        object ErrorInputTooShort : LogInEvent()
+        data class ErrorLogIn(val error: String) : LogInEvent()
+        object Success : LogInEvent()
+    }
+
+    sealed class LoadingState {
+        object Loading : LoadingState()
+        object NotLoading : LoadingState()
+    }
 }
